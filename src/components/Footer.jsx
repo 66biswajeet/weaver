ce0078b2d4ae3f7107da6777307e369b8fff451e
Ayuri016 <sahuayushi945@gmail.com>
2024-08-01 20:25:55 +0530

    Footer2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,24 @@ const Footer = () => {
           </a>
 
             </div>
-            <div className='ref'></div>
+            {/* <div className='ref'> */}
+              <div className='site'><h5>On our site</h5>
+              <span className='lists'>
+                <li><a href='/about'>About</a></li>
+                <li><a href='/about'>How it works</a></li>
+                <li><a href='/about'>Explore campaigns</a></li>
+                </span>
+              </div>
+              <div className='support'>
+              <h5>On our site</h5>
+              </div>
+              <div className='links'>
+              <h5>On our site</h5>
+              </div>
+              <div className='contact'>
+              <h5>On our site</h5>
+              </div>
+            {/* </div> */}
         </div>
     </footer>
   )
